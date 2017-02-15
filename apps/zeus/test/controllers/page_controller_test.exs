05e971aeb2f94701669c4ad6fbb4eefb9cf8edfd
Joel Byler <jbyler@covermymeds.com>
2017-02-14 21:53:31 -0500

    add color picker, to pass color to neopixel

--- apps/zeus/test/controllers/page_controller_test.exs
+++ apps/zeus/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Zeus.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Choose a color"
   end
 end
